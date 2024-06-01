14d4f1bc2aa8e93a34f7f7763c37bb1cf9ab3f72
interstellar-0614 <130843878+interstellar-0614@users.noreply.github.com>
2024-06-02 00:54:15 +0800

    Update page.tsx

--- app/admin_profile/page.tsx
+++ app/admin_profile/page.tsx
@@ -1,24 +1,6 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Button } from "@/components/ui/button"
 import { Badge } from "@/components/ui/badge"
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog"
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 import Link from "next/link"
 
 
@@ -42,50 +24,11 @@ const admin_profile = () => {
                 <p className="text-gray-600 text-sm">Admin ID</p>
             </div>
             <div className="flex flex-col items-center justify-center mt-8">
-                <form>
-                <Dialog>
-                    <DialogTrigger asChild>
-                        <Button className="px-12 w-60 up-primary-red">Update Information</Button>                    
-                    </DialogTrigger>
-                    <DialogContent className="sm:max-w-[425px]">
-                        <DialogHeader>
-                            <DialogTitle>Edit profile</DialogTitle>
-                            <DialogDescription>
-                                Make changes to your profile here. Click save when you&apos;re done.
-                            </DialogDescription>
-                        </DialogHeader>
-                        <div className="grid gap-4 py-4">
-                            <div className="grid grid-cols-4 items-center gap-4">
-                                <Label htmlFor="name" className="text-right">
-                                    Name
-                                </Label>
-                                <Input
-                                id="admin-name"
-                                className="col-span-3 border-black"
-                                />
-                            </div>
-                            <div className="grid grid-cols-4 items-center gap-4">
-                                <Label htmlFor="email" className="text-right">
-                                    Email
-                                </Label>
-                                <Input
-                                id="admin-email"
-                                className="col-span-3 border-black"
-                                />
-                            </div>
-                        </div>
-                        <DialogFooter>
-                            <Button type="submit" className="up-primary-red">Save changes</Button>
-                        </DialogFooter>
-                    </DialogContent>
-                </Dialog>
-                </form>
-                <Link href="/admin"><Button className="px-10 w-60 mt-4 up-primary-red">Manage Request</Button></Link>
+                <Link href="/admin_incident"><Button className="px-10 w-60 up-primary-red">Manage Incidents</Button></Link>                   
+                <Link href="/admin"><Button className="px-10 w-60 mt-4 up-primary-red">Update Inventory and Users</Button></Link>
             </div>
         </div>
 
     </div>
      );
 }
-
-export default admin_profile;
