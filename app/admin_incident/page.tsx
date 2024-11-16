404c646f9ac6134f3af39b7264e5c8da7eef15f1
interstellar-0614 <130843878+interstellar-0614@users.noreply.github.com>
2024-11-16 23:58:06 +0800

    Update page.tsx

--- app/admin_incident/page.tsx
+++ app/admin_incident/page.tsx
@@ -1,39 +1,20 @@
 import React, { useEffect, useState } from 'react'
 import { supabase } from '@/config/supabaseClient'
-import { DataTable } from '@/app/admin_incident/components/incident-data-table'
-import { Incident, columns } from "./components/incident-columns"
+import { IncidentDataTable } from '@/app/admin_incident/components/incident-data-table'
+import { Incident, incident_columns } from "@/app/admin_incident/components/incident-columns"
 
-
-async function getData(): Promise<Incident[]> {
-    // Fetch data from your API here.
-    return [
-      {
-        user: "Jeffer Mezo",
-        email: "m@example.com",
-        equipment: "Compound Microscope",
-        description: "Welding machine overheated, resulting in a small fire.",
-        reported_date: "7/05/23",
-        status: "Pending",
-      },
-
-      {
-        user: "Jeffrey Mezo",
-        email: "m@example.com",
-        equipment: "Vortex Mixer",
-        description: "Drill press stopped working mid-use, causing delay in production.",
-        reported_date: "7/27/23",
-        status: "Resolved",
-      },
-
-    ]
+const Incidentreport = async () => {
+  const { data: incidentData, error: incidentError} = await supabase.from('incidentform').select('*')
+  if (incidentError) {
+    console.error({incidentError });
+    return <div>Error loading data</div>;
   }
-   
-  export default async function DemoPage() {
-    const data = await getData()
-   
-    return (
-      <div className="container mx-auto py-10 pt-24">
-        <DataTable columns={columns} data={data} />
-      </div>
-    )
-  }
+  return (
+      
+    <div className="container mx-auto py-10 pt-24">
+      <IncidentDataTable columns={incident_columns} data={incidentData} />
+    </div>
+  )
+}
+ 
+export default Incidentreport;
