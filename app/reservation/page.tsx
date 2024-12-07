e3db139710bf348d4068a3cbd8caea320ef24307
JEFF4R <36539187+jeff-mezo@users.noreply.github.com>
2024-12-07 16:47:44 +0800

    reservation quantity fixed

--- app/reservation/page.tsx
+++ app/reservation/page.tsx
@@ -9,9 +9,11 @@ import { useQueryClient } from '@tanstack/react-query'
 import { ClipboardCheckIcon, ClipboardX, Trash } from 'lucide-react'
 import CartContext, { CartItem, Cart } from "@/app/equipmentpage/cartContext";
 import { supabase } from '@/config/supabaseClient'
+import { useRouter } from 'next/navigation'
 
-// type itemType = {
+// type CartItem = {
 //   id: string;
+//   eq_id: string;
 //   name: string;
 //   image: string;
 //   stock: number;
@@ -25,12 +27,16 @@ const Reservation: React.FC = () => {
 
   const { isFetching, data } = useUser();
   const queryClient = useQueryClient();
+  const router = useRouter();
 
-  const { addItemToCart, deleteItemFromCart, cart } = useContext(CartContext);
+
+  // CART DATA
+  const { addItemToCart, deleteItemFromCart, cart, clearCart } = useContext(CartContext);
 
   const increaseQty = (cartItem: CartItem) => {
     const newQty = cartItem?.quantity + 1;
     const item = { ...cartItem, quantity: newQty };
+    console.log("increaseQty: ", item);
 
     if (newQty > Number(cartItem.stock)) return;
 
@@ -40,21 +46,13 @@ const Reservation: React.FC = () => {
   const decreaseQty = (cartItem: CartItem) => {
     const newQty = cartItem?.quantity - 1;
     const item = { ...cartItem, quantity: newQty };
+    console.log("decreaseQty: ", item);
 
     if (newQty <= 0) return;
 
     addItemToCart(item);
   };
 
-  //console.log("1 item in the cart: ", cart)
-
-  // const [formData, setFormData] = useState<FormData>({
-  //   name: '',
-  //   studentNumber: '',
-  //   degreeProgram: '',
-  //   project: '',
-  //   adviser: '',
-  // });
 
   const [name, setName] = useState('');
   const [studno, setStudNo] = useState('');
@@ -62,7 +60,9 @@ const Reservation: React.FC = () => {
   const [project, setProject] = useState('');
   const [adviser, setAdviser] = useState('');
   const [error, setError] = useState<string | null>(null);
+
   // input values from session:
+
   useEffect(() => {
     if(data) {
         // if (!data) throw new Error('No user logged in');
@@ -75,11 +75,15 @@ const Reservation: React.FC = () => {
     } else if(error) {
         setError(error);
     }
-}, [data]);
+  }, [data]);
 
-  const [cartData, setCartData] = useState(cart.cartItems.map((item) => ({
-    ...item,
-  })))
+  // const [cartData, setCartData] = useState(cart.cartItems.map((item) => ({...item,})))
+
+  const [cartData, setCartData] = useState<CartItem[]>([]);
+
+  useEffect(() => {
+    setCartData(cart.cartItems.map((item) => ({...item,})));
+  }, [cart.cartItems]);
 
   //BORROW AND RETURN DATE ATTEMPT
   const [borrowDate, setBorrowDate] = useState('')
@@ -94,6 +98,11 @@ const Reservation: React.FC = () => {
     newCartData[index][field] = value
     setCartData(newCartData)
   }
+
+  // BYEBYE CART
+  // const clearCart = () => {
+  //   setCart({ ...cart, cartItems: [] }); // Assuming your cart state is managed like this
+  // };
   
   // const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
   //   const { id, value } = e.target;
@@ -104,6 +113,7 @@ const Reservation: React.FC = () => {
   // };
 
   const handleSubmit = async () => {
+
     try {
       console.log("submitting...")
 
@@ -152,13 +162,25 @@ const Reservation: React.FC = () => {
       console.log('cart: ', cartItemsData)
       if (cartItemsError) throw cartItemsError;
 
-      // // Handle success (e.g., show a success message or redirect)
-      // console.log('Checkout successful', { borrowerData, cartItemsData });
+      // clear cart in local storage
+      clearCart();
+
+      // redirect to profile
+      router.push("/profile");
+
+
     } catch (error) {
       // Handle error (e.g., show an error message)
       console.error('Checkout failed', error);
     }
   };
+
+  // const qtyCheck1 = () => {
+  //   console.log(cartData);
+  // }
+  // const qtyCheck2 = () => {
+  //   console.log(cart.cartItems);
+  // }
 
   
 
@@ -172,6 +194,12 @@ const Reservation: React.FC = () => {
           <p className="mt-2 text-md text-gray-600 dark:text-gray-400">
             Review your cart and complete your rental order.
           </p>
+          {/* <Button onClick={qtyCheck1}>
+            Check CartData
+          </Button>
+          <Button onClick={qtyCheck2}>
+            Check Cart.CartItem
+          </Button> */}
         </div>
         <div className="bg-white dark:bg-gray-800 shadow-sm rounded-lg overflow-hidden">
           <div className="px-4 py-5 sm:px-6">
@@ -361,3 +389,7 @@ const Reservation: React.FC = () => {
 }
 
 export default Reservation
+
+function redirectPath() {
+  throw new Error('Function not implemented.')
+}
