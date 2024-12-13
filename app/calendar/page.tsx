e6bac387e6b374e78c7bb171856ec466a14a9f0f
KanadeTachie <KBGaming22@yahoo.com>
2024-12-14 04:47:58 +0800

    Calendar
    DEC 14 2024 PATCH NOTES:
    - Calendar now works
    - Added ENTER key event listener (viewPDF)
    - Added fetchReservation function for calendar (clientActions)

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -1,34 +1,60 @@
-'use client'
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+/*
+DEC 14 2024 PATCH NOTES:
+- Calendar now works
 
-import React, { useEffect, useState } from 'react'
-import { supabase } from '@/config/supabaseClient'
-import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid'
-import timeGridPlugin from '@fullcalendar/timegrid'
+- Previous updates dev: KanadeTachie (King Behimino)
+- Current updates dev: KanadeTachie (King Behimino)
 
- 
+^^^Change as necessary to track progress
+*/ 
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import { fetchReservations } from '@/utils/clientActions';
+import { EventInput } from '@fullcalendar/core';
+
 export default function Calendar() {
+  const [events, setEvents] = useState<EventInput[]>([]);
 
+  useEffect(() => {
+    async function loadReservations() {
+      try {
+        const reservations = await fetchReservations();
+        setEvents(reservations); // Set events with name and email data
+      } catch (error) {
+        console.error('Error loading reservations:', error);
+      }
+    }
+    loadReservations(); // Fetch reservations on mount
+  }, []);
 
   return (
     <div className="mx-auto py-10 pt-24 w-9/12">
-     {/*<iframe className="mx-auto" style={{filter: 'hue-rotate(130deg)'}}src="https://calendar.google.com/calendar/embed?src=jpmezo%40up.edu.ph&ctz=Asia%2FManila" width="90%" height="80%" scrolling="no"></iframe> */}
-    
-     <FullCalendar
-      plugins={[ 
-        dayGridPlugin,
-        timeGridPlugin
-      ]}
-      headerToolbar={{
-        left: 'prev,next today',
-        center: 'title',
-        right: 'dayGridMonth,timeGridWeek,timeGridDay'
-      }}
-      initialView="dayGridMonth"
-      events={{}}
-      nowIndicator={true}
-      // eventClick={}
-    />    
+      <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin]}
+        headerToolbar={{
+          left: 'prev,next today',
+          center: 'title',
+          right: 'dayGridMonth,timeGridWeek,timeGridDay',
+        }}
+        initialView="dayGridMonth"
+        events={events} // Pass events to calendar
+        eventTimeFormat={{
+          hour: '2-digit',
+          minute: '2-digit',
+          meridiem: true,
+        }}
+        nowIndicator={true}
+        eventColor='maroon'
+        eventClick={(info) => {
+          alert(`Item Reserved: ${info.event.title}`);
+        }}
+      />
     </div>
-  )
+  );
 }
